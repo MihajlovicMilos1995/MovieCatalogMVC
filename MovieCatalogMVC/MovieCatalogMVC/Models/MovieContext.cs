73e38f3483680028a7a3f55cc49a2dcffb46a10e
Milos <mihajlovicmilos16@gmail.com>
2017-02-21 20:29:16 +0100

    Start

---
+++ MovieCatalogMVC/MovieCatalogMVC/Models/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovieCatalogMVC.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext() : base("MoviesDataBase")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
